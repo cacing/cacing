1259708b517713a96896194ecc27d95691cabeed
Hadi Hidayat Hammurabi <hadihammurabi@gmail.com>
2021-01-23 21:56:54 +0700

    add client-server socket

--- interface/socket/client.go
+++ interface/socket/client.go
@@ -1,9 +1,45 @@
 package socket
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"net"
+	"net/url"
+	"os"
+)
 
 // ConnectTo func
-func ConnectTo() error {
-	fmt.Println("connecting...")
-	return nil
+func ConnectTo(url *url.URL) error {
+	conn, err := net.Dial(connType, connHost+":"+url.Port())
+	if err != nil {
+		fmt.Println("Error connecting:", err.Error())
+		os.Exit(1)
+	}
+
+	password, _ := url.User.Password()
+	connectSignal := fmt.Sprintf("connect=>%s %s\n", url.User.Username(), password)
+	_, err = conn.Write([]byte(connectSignal))
+	if err != nil {
+		return err
+	}
+
+	// reader := bufio.NewReader(os.Stdin)
+
+	for {
+		rawMessage, _ := bufio.NewReader(conn).ReadString('\n')
+		// message := strings.Split(rawMessage, "=>")
+		log.Println(rawMessage)
+
+		// if message[0] == "success" {
+		// 	log.Println("> ", message)
+		// } else if message[0] == "error" {
+		// 	log.Println("Error > ", message)
+		// }
+
+		// fmt.Print("Text to send: ")
+		// input, _ := reader.ReadString('\n')
+		// conn.Write([]byte(input))
+	}
+
 }
--- interface/socket/server.go
+++ interface/socket/server.go
@@ -1,9 +1,97 @@
 package socket
 
-import "fmt"
+import (
+	"bufio"
+	"errors"
+	"fmt"
+	"net"
+	"os"
+	"strings"
+)
+
+const (
+	connHost = "localhost"
+	connType = "tcp"
+)
+
+// Config type
+type Config struct {
+	Port     string
+	Username string
+	Password string
+}
+
+// NewConfig return new server Config
+// or error if fail
+func NewConfig(port string, username string, password string) (*Config, error) {
+	if port == "" {
+		port = "8080"
+	}
+
+	if username == "" {
+		return nil, errors.New("Username can't be blank")
+	}
+
+	if password == "" {
+		return nil, errors.New("Password can't be blank")
+	}
+
+	config := &Config{
+		Port:     port,
+		Username: username,
+		Password: password,
+	}
+	return config, nil
+
+}
 
 // RunServer func
-func RunServer() error {
-	fmt.Println("server run")
+func RunServer(config *Config) error {
+	fmt.Println("Starting cacing server on " + connHost + ":" + config.Port)
+	l, err := net.Listen(connType, connHost+":"+config.Port)
+	if err != nil {
+		fmt.Println("Error listening:", err.Error())
+		os.Exit(1)
+	}
+	defer l.Close()
+
+	for {
+		c, err := l.Accept()
+		if err != nil {
+			fmt.Println("Error:", err.Error())
+			return nil
+		}
+
+		go handleConnection(c)
+	}
+}
+
+func handleConnection(conn net.Conn) {
+	buffer, err := bufio.NewReader(conn).ReadBytes('\n')
+
+	if err != nil {
+		fmt.Println("Client left.")
+		conn.Close()
+		return
+	}
+
+	clientMessage := strings.Split(string(buffer[:len(buffer)-1]), "=>")
+	if clientMessage[0] == "connect" {
+		user := strings.Split(clientMessage[1], " ")
+		err := authenticateClient(user[0], user[1])
+		if err != nil {
+			replySignal := fmt.Sprintf("error=>%s", err.Error())
+			conn.Write([]byte(replySignal))
+		} else {
+			fmt.Println("New connection")
+			replySignal := fmt.Sprintf("success=>connected")
+			conn.Write([]byte(replySignal))
+		}
+	}
+
+	handleConnection(conn)
+}
+
+func authenticateClient(username string, password string) error {
 	return nil
 }
