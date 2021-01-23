326b56008db2942836478d58c99df310e1380047
Hadi Hidayat Hammurabi <hadihammurabi@gmail.com>
2021-01-23 19:14:43 +0700

    test set and get data from ListStruct storage

--- storages/liststruct/liststruct_test.go
+++ storages/liststruct/liststruct_test.go
@@ -9,10 +9,30 @@ import (
 var initialDataSize = uint(0)
 var initialData = make([]*Data, initialDataSize)
 
+var key = "id"
+var val = uint(1238782738973)
+
 func TestCreateListStruct(t *testing.T) {
 	storage := NewListStruct(initialData)
 
 	if assert.NotNil(t, storage) {
 		assert.Equal(t, storage.GetSize(), initialDataSize)
+	}
+}
+
+func TestSetToListStruct(t *testing.T) {
+	storage := NewListStruct(initialData)
+	storage.Set(key, val)
+
+	assert.Equal(t, storage.GetSize(), uint(1))
+}
+
+func TestGetFromListStruct(t *testing.T) {
+	storage := NewListStruct(initialData)
+	storage.Set(key, val)
+
+	data, err := storage.Get(key)
+	if assert.Nil(t, err) {
+		assert.Equal(t, data, val)
 	}
 }
