a24c85afd2a99aa11fabdb57801acb719dab1141
Hadi Hidayat Hammurabi <hadihammurabi@gmail.com>
2021-01-23 21:57:00 +0700

    run socket using cli

--- interface/cli/cli.go
+++ interface/cli/cli.go
@@ -2,6 +2,7 @@ package cli
 
 import (
 	"cacing/interface/socket"
+	"cacing/utils"
 
 	"github.com/urfave/cli/v2"
 )
@@ -11,29 +12,48 @@ func NewCliApp(args []string) error {
 	c := &cli.App{
 		Name:  "cacing",
 		Usage: "Quite simple in memory cache storage",
-		Flags: []cli.Flag{
-			&cli.StringFlag{
-				Name:  "username",
-				Usage: "Username to access the storage server",
-			},
-			&cli.StringFlag{
-				Name:  "password",
-				Usage: "Secret password for username",
-			},
-		},
 		Commands: []*cli.Command{
 			{
 				Name:  "run",
 				Usage: "Start cacing server",
+				Flags: []cli.Flag{
+					&cli.StringFlag{
+						Name:  "port",
+						Usage: "Cacing server's host",
+					},
+					&cli.StringFlag{
+						Name:  "username",
+						Usage: "Username to access the storage server",
+					},
+					&cli.StringFlag{
+						Name:  "password",
+						Usage: "Secret password for username",
+					},
+				},
 				Action: func(ctx *cli.Context) error {
-					return socket.RunServer()
+					config := &socket.Config{
+						Port:     ctx.String("port"),
+						Username: ctx.String("username"),
+						Password: ctx.String("password"),
+					}
+					return socket.RunServer(config)
 				},
 			},
 			{
 				Name:  "connect",
 				Usage: "Connect to a cacing server as client",
+				Flags: []cli.Flag{
+					&cli.StringFlag{
+						Name:  "dsn",
+						Usage: "Cacing connection string",
+					},
+				},
 				Action: func(ctx *cli.Context) error {
-					return socket.ConnectTo()
+					url, err := utils.ParseURL(ctx.String("dsn"))
+					if err != nil {
+						return err
+					}
+					return socket.ConnectTo(url)
 				},
 			},
 		},
