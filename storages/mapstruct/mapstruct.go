b76556d914263fc2f939b1be2a5a246ee04e9ff2
Hadi Hidayat Hammurabi <hadihammurabi@gmail.com>
2021-01-24 11:48:35 +0700

    change list struct storage to map struct

---
+++ storages/mapstruct/mapstruct.go
@@ -0,0 +1,93 @@
+package mapstruct
+
+import (
+	"fmt"
+	"sync"
+	"time"
+
+	"github.com/hadihammurabi/cacing/storages"
+)
+
+// Data struct define structure of saved data
+type Data struct {
+	Value      interface{}
+	Expiration int64
+}
+
+// MapStruct is storage engine that store data into list of structs
+type MapStruct struct {
+	Data map[string]Data
+	Mu   sync.RWMutex
+}
+
+// NewMapStruct generate new MapStruct
+func NewMapStruct(initialData map[string]Data) storages.Storage {
+	return &MapStruct{
+		Data: initialData,
+	}
+}
+
+// GetSize return how many datum in storage
+func (store *MapStruct) GetSize() uint {
+	return uint(len(store.Data))
+}
+
+// Get return value with inserted key
+// if this key doesn't exists, return error
+func (store *MapStruct) Get(key string) (value interface{}, err error) {
+
+	store.Mu.RLock()
+
+	// check data expiration
+	data, exist := store.Data[key]
+	if exist && data.Expiration > 0 {
+		if time.Now().UnixNano() > data.Expiration {
+			delete(store.Data, key)
+		}
+	}
+
+	// get data after cleaned
+	data, exist = store.Data[key]
+	if !exist {
+		return nil, fmt.Errorf("data not found")
+	}
+
+	store.Mu.RUnlock()
+
+	return data.Value, nil
+
+}
+
+// Set to add data into storage
+// and return error if any problems happen
+func (store *MapStruct) Set(key string, val interface{}, t time.Duration) error {
+
+	store.Mu.Lock()
+	var ex int64
+	if t > 0 {
+		ex = time.Now().Add(t).UnixNano()
+	}
+
+	store.Data[key] = Data{
+		Value:      val,
+		Expiration: ex,
+	}
+	store.Mu.Unlock()
+
+	return nil
+}
+
+// Delete return deleted value
+// or error if any problems happen
+func (store *MapStruct) Delete(key string) (interface{}, error) {
+
+	store.Mu.Lock()
+	val, exist := store.Data[key]
+	if !exist {
+		return nil, fmt.Errorf("data not found")
+	}
+
+	delete(store.Data, key)
+	store.Mu.Unlock()
+	return val, nil
+}
--- storages/mapstruct/mapstruct_test.go
+++ storages/mapstruct/mapstruct_test.go
@@ -1,4 +1,4 @@
-package liststruct
+package mapstruct
 
 import (
 	"testing"
@@ -8,28 +8,28 @@ import (
 )
 
 var initialDataSize = uint(0)
-var initialData = map[string]Data{
+var initialData = map[string]Data{}
 
 var key = "id"
 var val = uint(1238782738973)
 
-func TestCreateListStruct(t *testing.T) {
-	storage := NewListStruct(initialData)
+func TestCreateMapStruct(t *testing.T) {
+	storage := NewMapStruct(initialData)
 
 	if assert.NotNil(t, storage) {
 		assert.Equal(t, storage.GetSize(), initialDataSize)
 	}
 }
 
-func TestSetToListStruct(t *testing.T) {
-	storage := NewListStruct(initialData)
-	storage.Set(key, val, 12 * time.Second)
+func TestSetToMapStruct(t *testing.T) {
+	storage := NewMapStruct(initialData)
+	storage.Set(key, val, 12*time.Second)
 
 	assert.Equal(t, storage.GetSize(), uint(1))
 }
 
-func TestGetFromListStruct(t *testing.T) {
-	storage := NewListStruct(initialData)
+func TestGetFromMapStruct(t *testing.T) {
+	storage := NewMapStruct(initialData)
 	storage.Set(key, val, 0)
 
 	data, err := storage.Get(key)
